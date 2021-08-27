25f9f6279308b683adfccdfedfdf51d65362693a
Nigel Eke <nigel@thinksmartit.com.au>
2021-08-27 17:18:51 +1000

    initial yew frontend - barebones

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,42 @@
-use crate::resolver::Resolver;
+use yew::prelude::*;
 
-mod expression;
-mod operator;
-mod resolver;
-mod result;
+struct App {
+    link: ComponentLink<Self>,
+    numbers: Vec<i32>,
+    goal: i32,
+}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        App {
+            link,
+            numbers: vec![],
+            goal: 0,
+        }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div class=classes!("w3-theme")>
+                <header class=classes!("w3-center", "w3-theme-dl", "w3-xxlarge")>{ "Letters and Numbers" }</header>
+                <div class=classes!("w3-center")>{ "Desktop" }</div>
+                <footer class=classes!("w3-bottom", "w3-center", "w3-theme-d3", "w3-tiny")>{ "Copyright (C) 2021; Nigel Eke. All rights reserved." }</footer>
+            </div>
+        }
+    }
+}
 
 fn main() {
-    let solutions = Resolver::find_solutions(&[3, 5, 5, 6, 1, 75], 559);
-    let no_solution = String::from("No solution!");
-    let solution = solutions.get(0).unwrap_or(&no_solution);
-    println!("{}", solution);
+    yew::start_app::<App>();
 }
